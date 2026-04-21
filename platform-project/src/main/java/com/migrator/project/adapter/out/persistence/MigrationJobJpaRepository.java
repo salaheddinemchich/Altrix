69694aa43f02272d32a1f2b0a02582fb705c967c
Salaheddine MCHICH <salaheddinemchich@gmail.com>
2026-04-21 11:24:47 +0100

    fix: remove postgres runtime data from git and clean misplaced classes #16

--- platform-project/src/main/java/com/migrator/project/adapter/out/persistence/MigrationJobJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.migrator.project.adapter.out.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.UUID;
-
-@Repository
-interface MigrationJobJpaRepository extends JpaRepository<MigrationJobEntity, UUID> {
-}
