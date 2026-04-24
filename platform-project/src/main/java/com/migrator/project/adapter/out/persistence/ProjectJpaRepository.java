1e70a103eaad4a05760b1ff5e098ed5209c73b23
Salaheddine MCHICH <salaheddinemchich@gmail.com>
2026-04-24 13:44:13 +0100

    feat: add CI pipeline, Docker setup, and reorganize adapters to out layer #30

--- platform-project/src/main/java/com/migrator/project/adapter/out/persistence/ProjectJpaRepository.java
+++ platform-project/src/main/java/com/migrator/project/adapter/out/persistence/ProjectJpaRepository.java
@@ -1,4 +1,4 @@
-package com.migrator.project.adapter.persistence;
+package com.migrator.project.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- platform-project/src/main/java/com/migrator/project/adapter/out/persistence/ProjectPersistenceAdapter.java
+++ platform-project/src/main/java/com/migrator/project/adapter/out/persistence/ProjectPersistenceAdapter.java
@@ -1,4 +1,4 @@
-package com.migrator.project.adapter.persistence;
+package com.migrator.project.adapter.out.persistence;
 
 import com.migrator.project.domain.model.Project;
 import com.migrator.project.domain.port.out.ProjectRepository;
